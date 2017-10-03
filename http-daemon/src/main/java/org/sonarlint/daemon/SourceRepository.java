485bdd21738553dbbfe83316f999b46523639575
Daniel Schwarz <daniel.schwarz@sonarsource.com>
2017-10-03 10:36:19 +0200

    store data in file system

--- http-daemon/src/main/java/org/sonarlint/daemon/SourceRepository.java
+++ http-daemon/src/main/java/org/sonarlint/daemon/SourceRepository.java
@@ -20,98 +20,65 @@
 package org.sonarlint.daemon;
 
 import java.io.IOException;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.UUID;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpHost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.message.BasicHeader;
-import org.elasticsearch.action.get.GetRequest;
-import org.elasticsearch.action.get.GetResponse;
-import org.elasticsearch.action.index.IndexRequest;
-import org.elasticsearch.client.RestClient;
-import org.elasticsearch.client.RestHighLevelClient;
-
-import static org.apache.http.entity.ContentType.APPLICATION_JSON;
-import static org.apache.http.protocol.HTTP.CONTENT_TYPE;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.logging.Logger;
 
 public class SourceRepository {
 
-  private RestClient lowLevelClient;
-  private RestHighLevelClient highLevelClient;
+  public static final Charset CHARSET = StandardCharsets.UTF_8;
+  public static final Logger LOGGER = Logger.getLogger(SourceRepository.class.getName());
 
-  public String load(String uuid) {
-    init();
+  private Path storeDir;
+
+  public void init(Path workDir) {
+    if (storeDir == null) {
+      storeDir = workDir.resolve("store");
+      try {
+        Files.createDirectories(storeDir);
+      } catch (IOException e) {
+        e.printStackTrace();
+        throw new IllegalStateException("Cannot create store");
+      }
+      LOGGER.info(() -> "storeDir: " + storeDir.toAbsolutePath());
+    }
+  }
+
+  public String load(String storedAs) {
+    RandomString.validate(storedAs);
     try {
-      GetResponse response = highLevelClient.get(new GetRequest("files", "file", uuid).storedFields("code"));
-      return (String) response.getField("code").getValue();
+      return new String(Files.readAllBytes(getFile(storedAs)), CHARSET);
     } catch (IOException e) {
-      throw new IllegalStateException("Cannot load "+uuid, e);
+      e.printStackTrace();
+      throw new IllegalStateException("Cannot read file");
     }
   }
 
   public String store(String code) {
-    init();
-    String storedAs = UUID.randomUUID().toString();
+    String storedAs = RandomString.generate();
     try {
-      HashMap<Object, Object> document = new HashMap<>();
-      document.put("code", code);
-      highLevelClient.index(new IndexRequest("files", "file", storedAs).source(document));
+      Files.write(getFile(storedAs), code.getBytes(CHARSET), StandardOpenOption.CREATE);
     } catch (IOException e) {
-      throw new IllegalStateException("Cannot save", e);
+      e.printStackTrace();
+      throw new IllegalStateException("Cannot write file");
     }
     return storedAs;
   }
 
-  public void init() {
-    if (lowLevelClient != null) {
-      return;
-    }
-    lowLevelClient = RestClient.builder(new HttpHost("localhost", 9200, "http")).build();
+  private Path getFile(String storedAs) {
+
+    // avoid having too many files in one directory
+    Path dir = storeDir.resolve(storedAs.substring(0, 2));
+
     try {
-      highLevelClient = new RestHighLevelClient(lowLevelClient);
-      createIndexIfNeeded();
-    } catch (Exception e) {
-      try {
-        lowLevelClient.close();
-      } catch (Exception e1) {
-        // ignore this one
-      }
-      throw new IllegalStateException("Cannot initialize the connection to Elasticsearch", e);
+      Files.createDirectories(dir);
+    } catch (IOException e) {
+      e.printStackTrace();
+      throw new IllegalStateException("Cannot create store subdir");
     }
-  }
-
-  private void createIndexIfNeeded() throws IOException {
-    int indexExists = lowLevelClient.performRequest("HEAD", "/files").getStatusLine().getStatusCode();
-
-    if (indexExists == 200) {
-      return;
-    }
-
-    if (indexExists == 404) {
-      createIndex();
-      return;
-    }
-
-    throw new IllegalStateException("Unexpected response from Elasticsearch: " + indexExists);
-  }
-
-  private void createIndex() throws IOException {
-    lowLevelClient.performRequest("PUT", "/files");
-    HttpEntity fileMapping = new StringEntity("{\n" +
-      "      \"properties\": {\n" +
-      "        \"code\": {\n" +
-      "          \"type\": \"keyword\",\n" +
-      "          \"index\": \"false\",\n" +
-      "          \"store\": \"true\"\n" +
-      "        }\n" +
-      "      }\n" +
-      "  }");
-    lowLevelClient.performRequest("PUT", "/files/_mapping/file", Collections.emptyMap(), fileMapping, new BasicHeader(CONTENT_TYPE, APPLICATION_JSON.getMimeType()));
-  }
-
-  private void dropIndex() throws IOException {
-    lowLevelClient.performRequest("DELETE", "/files");
+    return dir.resolve(storedAs);
   }
 }
