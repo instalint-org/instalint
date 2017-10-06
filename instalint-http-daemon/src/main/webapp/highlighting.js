69c4bcf760ae229ba07d93d394506d49189aeda8
Elena Vilchik <vilchik.elena@gmail.com>
2017-10-06 14:24:37 +0200

    Move button for link

--- instalint-http-daemon/src/main/webapp/highlighting.js
+++ instalint-http-daemon/src/main/webapp/highlighting.js
@@ -1,7 +1,7 @@
 
-// test cases: https://jsfiddle.net/p9vdzhtd/14/
+// test cases: https://jsfiddle.net/p9vdzhtd/25/
 function lineOffsets(code) {
-	var offsets = [];
+    var offsets = [];
     var pos = 0;
     code.split("\n").forEach(line => {
         offsets.push(pos);
@@ -28,21 +28,33 @@ function doFormat(response) {
 
     var issueStart = [];
     var issueEnd = [];
-    response.issues.map(issue => issue.textRange).forEach(highlight => {
+    var issueBoxes = [];
+    response.issues.forEach(issue => {
+        var highlight = issue.textRange;
         var start = offsets[highlight.startLine - 1] + highlight.startOffset;
+        var endOfLine = offsets[highlight.startLine];
         var end = offsets[highlight.endLine - 1] + highlight.endOffset;
         issueStart[start] = 1;
         issueEnd[end] = 1;
+        if (issue.message) {
+            issueBoxes[endOfLine] = (issueBoxes[endOfLine] || []);
+            issueBoxes[endOfLine].push(issue.message);
+        }
     });
 
     var characterIndex;
     var result = "";
-    for (characterIndex = 0; characterIndex <= code.length; characterIndex++) {
+    for (characterIndex = 0; characterIndex <= code.length + 1; characterIndex++) {
         if (lineHighlightEnd[characterIndex]) {
             result += '</span>';
         }
         if (issueEnd[characterIndex]) {
             result += '</span>';
+        }
+        if (issueBoxes[characterIndex]) {
+            issueBoxes[characterIndex].forEach(message => {
+                result += '<div class="issue">' + message + '</div>';
+            });
         }
         if (issueStart[characterIndex]) {
             result += '<span class="source-line-code-issue">';
